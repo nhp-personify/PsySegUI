42454ffe9586f88529680eec7d3e2e99c5eb78ca
nhp-personify <67583218+nhp-personify@users.noreply.github.com>
2021-04-20 16:26:08 +0700

    Updates

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/styles.css';
-
-//const template = <p>Welcome to PSY</p>;
-
-//ReactDOM.render(template, document.getElementById('root'));
