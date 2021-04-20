d5f0403a64e7eb06468fdc0d0a580760d2cc6260
nhp-personify <67583218+nhp-personify@users.noreply.github.com>
2021-04-20 16:21:59 +0700

    Temporary UI

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+
+//const template = <p>Welcome to PSY</p>;
+
+//ReactDOM.render(template, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      // New rules to load css
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
